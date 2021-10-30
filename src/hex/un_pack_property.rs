fb0ef25c707d026b0aedca2187162580b14c98a1
kumatata <chinagxwei@gmail.com>
2021-10-30 09:04:57 +0800

    change project

--- src/hex/un_pack_property.rs
+++ src/hex/un_pack_property.rs
@@ -1,26 +1,31 @@
 use crate::hex::{PropertyItem, Property};
 use std::convert::TryFrom;
+use std::option::Option::Some;
+
+
+fn handle_property<'a>(property: Property, properties: &mut Vec<PropertyItem>, length: &mut u32, data: &'a [u8]) -> Option<&'a [u8]> {
+    if let Some((item, last_data)) = property.unpack_property_handle(length, data.get(1..).unwrap()) {
+        properties.push(item);
+        return Some(last_data);
+    }
+    None
+}
 
 pub fn connect(mut length: u32, mut data: &[u8]) -> Vec<PropertyItem> {
     let mut properties = vec![];
-    loop {
+
+    while length > 0 {
         let property = data[0];
         match Property::try_from(property) {
             Ok(p) => {
                 if p.is_connect_property() {
-                    if let Some((item, last_data)) = p.unpack_property_handle(&mut length, data.get(1..).unwrap()) {
-                        data = last_data;
-                        properties.push(item);
-                    }
+                    if let Some(v) = handle_property(p, &mut properties, &mut length, data.get(1..).unwrap()) { data = v; }
                 }
             }
             Err(e) => {
-                println!("Property {:?} not exist", e)
+                println!("Property {:?} not exist", e);
+                break;
             }
-        }
-
-        if length <= 0 {
-            break;
         }
     }
     properties
@@ -28,24 +33,18 @@ pub fn connect(mut length: u32, mut data: &[u8]) -> Vec<PropertyItem> {
 
 pub fn connack(mut length: u32, mut data: &[u8]) -> Vec<PropertyItem> {
     let mut properties = vec![];
-    loop {
+    while length > 0 {
         let property = data[0];
         match Property::try_from(property) {
             Ok(p) => {
                 if p.is_connack_property() {
-                    if let Some((item, last_data)) = p.unpack_property_handle(&mut length, data.get(1..).unwrap()) {
-                        data = last_data;
-                        properties.push(item);
-                    }
+                    if let Some(v) = handle_property(p, &mut properties, &mut length, data.get(1..).unwrap()) { data = v; }
                 }
             }
             Err(e) => {
-                println!("Property {:?} not exist", e)
+                println!("Property {:?} not exist", e);
+                break;
             }
-        }
-
-        if length <= 0 {
-            break;
         }
     }
 
@@ -54,24 +53,18 @@ pub fn connack(mut length: u32, mut data: &[u8]) -> Vec<PropertyItem> {
 
 pub fn publish(mut length: u32, mut data: &[u8]) -> Vec<PropertyItem> {
     let mut properties = vec![];
-    loop {
+    while length > 0 {
         let property = data[0];
         match Property::try_from(property) {
             Ok(p) => {
                 if p.is_publish_property() {
-                    if let Some((item, last_data)) = p.unpack_property_handle(&mut length, data.get(1..).unwrap()) {
-                        data = last_data;
-                        properties.push(item);
-                    }
+                    if let Some(v) = handle_property(p, &mut properties, &mut length, data.get(1..).unwrap()) { data = v; }
                 }
             }
             Err(e) => {
-                println!("Property {:?} not exist", e)
+                println!("Property {:?} not exist", e);
+                break;
             }
-        }
-
-        if length <= 0 {
-            break;
         }
     }
 
@@ -80,24 +73,18 @@ pub fn publish(mut length: u32, mut data: &[u8]) -> Vec<PropertyItem> {
 
 pub fn subscribe(mut length: u32, mut data: &[u8]) -> Vec<PropertyItem> {
     let mut properties = vec![];
-    loop {
+    while length > 0 {
         let property = data[0];
         match Property::try_from(property) {
             Ok(p) => {
                 if p.is_subscribe_property() {
-                    if let Some((item, last_data)) = p.unpack_property_handle(&mut length, data.get(1..).unwrap()) {
-                        data = last_data;
-                        properties.push(item);
-                    }
+                    if let Some(v) = handle_property(p, &mut properties, &mut length, data.get(1..).unwrap()) { data = v; }
                 }
             }
             Err(e) => {
-                println!("Property {:?} not exist", e)
+                println!("Property {:?} not exist", e);
+                break;
             }
-        }
-
-        if length <= 0 {
-            break;
         }
     }
 
@@ -106,24 +93,18 @@ pub fn subscribe(mut length: u32, mut data: &[u8]) -> Vec<PropertyItem> {
 
 pub fn unsubscribe(mut length: u32, mut data: &[u8]) -> Vec<PropertyItem> {
     let mut properties = vec![];
-    loop {
+    while length > 0 {
         let property = data[0];
         match Property::try_from(property) {
             Ok(p) => {
                 if p.is_unsubscribe_property() {
-                    if let Some((item, last_data)) = p.unpack_property_handle(&mut length, data.get(1..).unwrap()) {
-                        data = last_data;
-                        properties.push(item);
-                    }
+                    if let Some(v) = handle_property(p, &mut properties, &mut length, data.get(1..).unwrap()) { data = v; }
                 }
             }
             Err(e) => {
-                println!("Property {:?} not exist", e)
+                println!("Property {:?} not exist", e);
+                break;
             }
-        }
-
-        if length <= 0 {
-            break;
         }
     }
 
@@ -132,24 +113,18 @@ pub fn unsubscribe(mut length: u32, mut data: &[u8]) -> Vec<PropertyItem> {
 
 pub fn suback(mut length: u32, mut data: &[u8]) -> Vec<PropertyItem> {
     let mut properties = vec![];
-    loop {
+    while length > 0 {
         let property = data[0];
         match Property::try_from(property) {
             Ok(p) => {
                 if p.is_pub_and_sub_property() {
-                    if let Some((item, last_data)) = p.unpack_property_handle(&mut length, data.get(1..).unwrap()) {
-                        data = last_data;
-                        properties.push(item);
-                    }
+                    if let Some(v) = handle_property(p, &mut properties, &mut length, data.get(1..).unwrap()) { data = v; }
                 }
             }
             Err(e) => {
-                println!("Property {:?} not exist", e)
+                println!("Property {:?} not exist", e);
+                break;
             }
-        }
-
-        if length <= 0 {
-            break;
         }
     }
 
@@ -158,24 +133,18 @@ pub fn suback(mut length: u32, mut data: &[u8]) -> Vec<PropertyItem> {
 
 pub fn unsuback(mut length: u32, mut data: &[u8]) -> Vec<PropertyItem> {
     let mut properties = vec![];
-    loop {
+    while length > 0 {
         let property = data[0];
         match Property::try_from(property) {
             Ok(p) => {
                 if p.is_pub_and_sub_property() {
-                    if let Some((item, last_data)) = p.unpack_property_handle(&mut length, data.get(1..).unwrap()) {
-                        data = last_data;
-                        properties.push(item);
-                    }
+                    if let Some(v) = handle_property(p, &mut properties, &mut length, data.get(1..).unwrap()) { data = v; }
                 }
             }
             Err(e) => {
-                println!("Property {:?} not exist", e)
+                println!("Property {:?} not exist", e);
+                break;
             }
-        }
-
-        if length <= 0 {
-            break;
         }
     }
 
@@ -184,24 +153,18 @@ pub fn unsuback(mut length: u32, mut data: &[u8]) -> Vec<PropertyItem> {
 
 pub fn disconnect(mut length: u32, mut data: &[u8]) -> Vec<PropertyItem> {
     let mut properties = vec![];
-    loop {
+    while length > 0 {
         let property = data[0];
         match Property::try_from(property) {
             Ok(p) => {
                 if p.is_disconnect_property() {
-                    if let Some((item, last_data)) = p.unpack_property_handle(&mut length, data.get(1..).unwrap()) {
-                        data = last_data;
-                        properties.push(item);
-                    }
+                    if let Some(v) = handle_property(p, &mut properties, &mut length, data.get(1..).unwrap()) { data = v; }
                 }
             }
             Err(e) => {
-                println!("Property {:?} not exist", e)
+                println!("Property {:?} not exist", e);
+                break;
             }
-        }
-
-        if length <= 0 {
-            break;
         }
     }
 
@@ -210,24 +173,18 @@ pub fn disconnect(mut length: u32, mut data: &[u8]) -> Vec<PropertyItem> {
 
 pub fn auth(mut length: u32, mut data: &[u8]) -> Vec<PropertyItem> {
     let mut properties = vec![];
-    loop {
+    while length > 0 {
         let property = data[0];
         match Property::try_from(property) {
             Ok(p) => {
                 if p.is_auth_property() {
-                    if let Some((item, last_data)) = p.unpack_property_handle(&mut length, data.get(1..).unwrap()) {
-                        data = last_data;
-                        properties.push(item);
-                    }
+                    if let Some(v) = handle_property(p, &mut properties, &mut length, data.get(1..).unwrap()) { data = v; }
                 }
             }
             Err(e) => {
-                println!("Property {:?} not exist", e)
+                println!("Property {:?} not exist", e);
+                break;
             }
-        }
-
-        if length <= 0 {
-            break;
         }
     }
 
@@ -236,24 +193,18 @@ pub fn auth(mut length: u32, mut data: &[u8]) -> Vec<PropertyItem> {
 
 pub fn pub_and_sub(mut length: u32, mut data: &[u8]) -> Vec<PropertyItem> {
     let mut properties = vec![];
-    loop {
+    while length > 0 {
         let property = data[0];
         match Property::try_from(property) {
             Ok(p) => {
                 if p.is_pub_and_sub_property() {
-                    if let Some((item, last_data)) = p.unpack_property_handle(&mut length, data.get(1..).unwrap()) {
-                        data = last_data;
-                        properties.push(item);
-                    }
+                    if let Some(v) = handle_property(p, &mut properties, &mut length, data.get(1..).unwrap()) { data = v; }
                 }
             }
             Err(e) => {
-                println!("Property {:?} not exist", e)
+                println!("Property {:?} not exist", e);
+                break;
             }
-        }
-
-        if length <= 0 {
-            break;
         }
     }
 
@@ -262,24 +213,18 @@ pub fn pub_and_sub(mut length: u32, mut data: &[u8]) -> Vec<PropertyItem> {
 
 pub fn will_properties(mut length: u32, mut data: &[u8]) -> Vec<PropertyItem> {
     let mut properties = vec![];
-    loop {
+    while length > 0 {
         let property = data[0];
         match Property::try_from(property) {
             Ok(p) => {
                 if p.is_will_property() {
-                    if let Some((item, last_data)) = p.unpack_property_handle(&mut length, data.get(1..).unwrap()) {
-                        data = last_data;
-                        properties.push(item);
-                    }
+                    if let Some(v) = handle_property(p, &mut properties, &mut length, data.get(1..).unwrap()) { data = v; }
                 }
             }
             Err(e) => {
-                println!("Property {:?} not exist", e)
+                println!("Property {:?} not exist", e);
+                break;
             }
-        }
-
-        if length <= 0 {
-            break;
         }
     }
 
